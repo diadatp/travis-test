0d43fc2487cd285485f48ce7d6f0259670bee519
diadatp <admin@diadatp.com>
2015-07-04 20:38:19 +0530

    Testing alternative way to make comments in bash files. Making nginx report it's compiled modules.

--- build_nginx.sh
+++ build_nginx.sh
@@ -3,17 +3,17 @@
 cd nginx
 
 ./configure --prefix="../build" \
-			# --sbin-path="bin/nginx" \
-			# --conf-path=/usr/local/nginx/nginx.conf \
-			# --pid-path=/usr/local/nginx/nginx.pid \
-			# --error-log-path=path \
-			# --http-log-path=path \
-			# --user=www \
-			# --group=www \
+#			--sbin-path="bin/nginx" \
+#			--conf-path=/usr/local/nginx/nginx.conf \
+#			--pid-path=/usr/local/nginx/nginx.pid \
+#			--error-log-path=path \
+#			--http-log-path=path \
+#			--user=www \
+#			--group=www \
 			--with-http_ssl_module \
-			# --with-pcre=../pcre-4.4 \
+#			--with-pcre=../pcre-4.4 \
 			--with-pcre-jit \
-			# --with-zlib=../zlib-1.1.3 \
+#			--with-zlib=../zlib-1.1.3 \
 			--with-file-aio \
 			--with-ipv6 \
 			--with-http_ssl_module \
@@ -21,12 +21,12 @@ cd nginx
 			--with-http_realip_module \
 			--with-http_addition_module \
 			--with-http_xslt_module \
-			# --with-http_image_filter_module \
+#			--with-http_image_filter_module \
 			--with-http_geoip_module \
 			--with-http_sub_module \
-			# --with-http_dav_module \
-			# --with-http_flv_module \
-			# --with-http_mp4_module \
+#			--with-http_dav_module \
+#			--with-http_flv_module \
+#			--with-http_mp4_module \
 			--with-http_gunzip_module \
 			--with-http_gzip_static_module \
 			--with-http_random_index_module \
@@ -42,3 +42,6 @@ cd nginx
 
 make
 make install
+
+../build/nginx -V
+
