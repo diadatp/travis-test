9639d38c9f6b26b9f84083ee77a31b412740cf4e
diadatp <admin@diadatp.com>
2015-07-04 21:01:42 +0530

    Changed the order of options to configure.

--- build_nginx.sh
+++ build_nginx.sh
@@ -3,17 +3,8 @@
 cd nginx
 
 ./configure --prefix="../build" \
-#			--sbin-path="bin/nginx" \
-#			--conf-path=/usr/local/nginx/nginx.conf \
-#			--pid-path=/usr/local/nginx/nginx.pid \
-#			--error-log-path=path \
-#			--http-log-path=path \
-#			--user=www \
-#			--group=www \
 			--with-http_ssl_module \
-#			--with-pcre=../pcre-4.4 \
 			--with-pcre-jit \
-#			--with-zlib=../zlib-1.1.3 \
 			--with-file-aio \
 			--with-ipv6 \
 			--with-http_ssl_module \
@@ -21,12 +12,8 @@ cd nginx
 			--with-http_realip_module \
 			--with-http_addition_module \
 			--with-http_xslt_module \
-#			--with-http_image_filter_module \
 			--with-http_geoip_module \
 			--with-http_sub_module \
-#			--with-http_dav_module \
-#			--with-http_flv_module \
-#			--with-http_mp4_module \
 			--with-http_gunzip_module \
 			--with-http_gzip_static_module \
 			--with-http_random_index_module \
@@ -39,6 +26,19 @@ cd nginx
 			--with-pcre \
 			--with-google_perftools_module \
 			--with-debug
+#			--with-http_dav_module \
+#			--with-http_flv_module \
+#			--with-http_mp4_module \
+#			--with-zlib=../zlib-1.1.3 \
+#			--with-pcre=../pcre-4.4 \
+#			--with-http_image_filter_module \
+#			--sbin-path="bin/nginx" \
+#			--conf-path=/usr/local/nginx/nginx.conf \
+#			--pid-path=/usr/local/nginx/nginx.pid \
+#			--error-log-path=path \
+#			--http-log-path=path \
+#			--user=www \
+#			--group=www \
 
 make
 make install
