99c9830d3053775172029f1a828c7d65c3f021d4
diadatp <admin@diadatp.com>
2015-07-05 18:28:15 +0530

    Lets build PHP.

--- build_nginx.sh
+++ build_nginx.sh
@@ -11,8 +11,6 @@ cd nginx
 			--with-http_spdy_module \
 			--with-http_realip_module \
 			--with-http_addition_module \
-			--with-http_xslt_module \
-			--with-http_geoip_module \
 			--with-http_sub_module \
 			--with-http_gunzip_module \
 			--with-http_gzip_static_module \
@@ -20,12 +18,14 @@ cd nginx
 			--with-http_secure_link_module \
 			--with-http_degradation_module \
 			--with-http_stub_status_module \
-			--with-http_perl_module \
 			--with-mail \
 			--with-mail_ssl_module \
 			--with-pcre \
 			--with-google_perftools_module \
 			--with-debug
+#			--with-http_geoip_module \
+#			--with-http_xslt_module \
+#			--with-http_perl_module \
 #			--with-http_dav_module \
 #			--with-http_flv_module \
 #			--with-http_mp4_module \
---
+++ build_php.sh
@@ -0,0 +1,1 @@
+build_php.sh
