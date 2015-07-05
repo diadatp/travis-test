7478fc671425aa9c5b387f6f248951026807463a
diadatp <admin@diadatp.com>
2015-07-05 20:40:55 +0530

    Fixed path issues in prefix. Made compilation, testing and install steps conditional.

--- build_php.sh
+++ build_php.sh
@@ -3,12 +3,11 @@
 cd php-fpm
 
 ./buildconf --force
-./configure --prefix="$(pwd)/build" \
+./configure --prefix="$(pwd)/../build" \
 			--enable-fpm \
 			--with-mysql
 
-make -j$(cat /proc/cpuinfo | grep "cpu cores" | cut -f3 -d' ' | head -n 1)
-make install
+make -j$(cat /proc/cpuinfo | grep "cpu cores" | cut -f3 -d' ' | head -n 1) && make test && make install
 
 cd ..
 ./build/bin/php -i
