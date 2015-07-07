69ed55141fc72832cdf11c7aa050a3e543ed8392
diadatp <admin@diadatp.com>
2015-07-07 18:53:20 +0530

    Pulling travis from repo. Lets go prebuilt.

--- build_hhvm.sh
+++ build_hhvm.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 
+mkdir hhvm
 cd hhvm
 
-git submodule update --init --recursive
-
-cmake -DENABLE_MCROUTER=false .
-make
-
-cd ..
+wget http://dl.hhvm.com/ubuntu/pool/main/h/hhvm/hhvm_$HHVM_VER~trusty_amd64.deb
+ar vx hhvm_$HHVM_VER~trusty_amd64.deb
+rm hhvm_$HHVM_VER~trusty_amd64.deb
+tar -xzvf data.tar.gz --directory ../build
