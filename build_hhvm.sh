41d0236adc4aca4c0a6d51973ed4264a6b225227
diadatp <admin@diadatp.com>
2015-07-08 10:56:15 +0530

    Fixed a typo. Was untaring the wrong file.

--- build_hhvm.sh
+++ build_hhvm.sh
@@ -5,4 +5,4 @@ cd hhvm
 wget http://dl.hhvm.com/ubuntu/pool/main/h/hhvm/hhvm_$HHVM_VER~trusty_amd64.deb
 ar vx hhvm_$HHVM_VER~trusty_amd64.deb
 # rm hhvm_$HHVM_VER~trusty_amd64.deb debian-binary control.tar.gz _gpgbuilder
-tar -xvf data.tar.gz --directory ../build
+tar -xvf data.tar.xz --directory ../build
