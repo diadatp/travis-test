c3bd9b58b30cdd9f87646b1dff7c42191d19cb74
diadatp <admin@diadatp.com>
2015-07-14 15:36:19 +0530

    Small BASH fixes.

--- build_hhvm.sh
+++ build_hhvm.sh
@@ -3,14 +3,11 @@
 cd hhvm
 
 # how many virtual processors are there?
-export NUMCPUS=$(cat /proc/cpuinfo | grep "cpu cores" | cut -f3 -d' ' | head -n 1)
-
-# parallel make
-alias pmake='ionice -c3 nice -n 19 make -j$NUMCPUS --load-average=$NUMCPUS'
+export NUMCPUS=$(grep ^processor /proc/cpuinfo | wc -l)
 
 git submodule update --init --recursive
 cmake .
-pmake
+ionice -c3 nice -n 19 make -j$NUMCPUS --load-average=$NUMCPUS
 
 # wget http://dl.hhvm.com/ubuntu/pool/main/h/hhvm/hhvm_$HHVM_VER~trusty_amd64.deb
 # ar vx hhvm_$HHVM_VER~trusty_amd64.deb
