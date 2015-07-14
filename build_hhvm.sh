98ed7707a37b7f3617473bf00ba34fd77aeb64f8
diadatp <admin@diadatp.com>
2015-07-14 14:37:22 +0530

    Testing parallel build for hhvm.

--- build_hhvm.sh
+++ build_hhvm.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-mkdir hhvm
 cd hhvm
 
-wget http://dl.hhvm.com/ubuntu/pool/main/h/hhvm/hhvm_$HHVM_VER~trusty_amd64.deb
-ar vx hhvm_$HHVM_VER~trusty_amd64.deb
-# rm hhvm_$HHVM_VER~trusty_amd64.deb debian-binary control.tar.gz _gpgbuilder
-tar -xvf data.tar.xz --directory ../build
+# how many virtual processors are there?
+export NUMCPUS=$(cat /proc/cpuinfo | grep "cpu cores" | cut -f3 -d' ' | head -n 1)
+
+# parallel make
+alias pmake='ionice -c3 nice -n 19 make -j$NUMCPUS --load-average=$NUMCPUS'
+
+git submodule update --init --recursive
+cmake .
+pmake
+
+# wget http://dl.hhvm.com/ubuntu/pool/main/h/hhvm/hhvm_$HHVM_VER~trusty_amd64.deb
+# ar vx hhvm_$HHVM_VER~trusty_amd64.deb
+# tar -xvf data.tar.xz --directory ../build
